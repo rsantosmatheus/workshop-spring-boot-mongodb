9fbdfd9edd12b2876690233a7a5eb5cdc9b36160
Matheus <rsantos.matheus@outlook.com>
2020-10-13 16:02:11 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/SpringbootMongoDb/course/repository/PostRepository.java
@@ -0,0 +1,13 @@
+package com.SpringbootMongoDb.course.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.SpringbootMongoDb.course.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+	
+
+
+}
