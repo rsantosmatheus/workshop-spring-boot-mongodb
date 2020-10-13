cfa6444c84c447b112ffece6ec0ffbb556369314
Matheus <rsantos.matheus@outlook.com>
2020-10-13 16:23:39 -0300

    Projeção dos dados do autor com DTO

---
+++ src/main/java/com/SpringbootMongoDb/course/DTO/AuthorDTO.java
@@ -0,0 +1,40 @@
+package com.SpringbootMongoDb.course.DTO;
+
+import java.io.Serializable;
+
+import com.SpringbootMongoDb.course.domain.User;
+
+public class AuthorDTO implements Serializable{
+	
+
+	private static final long serialVersionUID = 1L;
+	public String id;
+	public String name;
+	
+	public AuthorDTO() {
+	}
+	
+	public AuthorDTO(User obj) {
+		id= obj.getId();
+		name = obj.getName();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
--- src/main/java/com/SpringbootMongoDb/course/config/Instantiation.java
+++ src/main/java/com/SpringbootMongoDb/course/config/Instantiation.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.SpringbootMongoDb.course.DTO.AuthorDTO;
 import com.SpringbootMongoDb.course.domain.Post;
 import com.SpringbootMongoDb.course.domain.User;
 import com.SpringbootMongoDb.course.repository.PostRepository;
@@ -35,11 +36,11 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");	
 		
-		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo, Abraços", maria);
-		
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Hoje eu acordei feliz!", maria);
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo, Abraços",new AuthorDTO(maria));
+		
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Hoje eu acordei feliz!", new AuthorDTO(maria));
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
