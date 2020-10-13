1958f129928d42fca0293adf0d2037a936079410
Matheus <rsantos.matheus@outlook.com>
2020-10-13 14:54:03 -0300

    Obtendo um usuário por id

--- src/main/java/com/SpringbootMongoDb/course/services/UserService.java
+++ src/main/java/com/SpringbootMongoDb/course/services/UserService.java
@@ -1,22 +1,27 @@
 package com.SpringbootMongoDb.course.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.SpringbootMongoDb.course.domain.User;
 import com.SpringbootMongoDb.course.repository.UserRepository;
+import com.SpringbootMongoDb.course.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
-	
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 }
---
+++ src/main/java/com/SpringbootMongoDb/course/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,12 @@
+package com.SpringbootMongoDb.course.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
